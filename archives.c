dfec0f318b46ead408562c0b2d0a829b62c81f6e
PedroHenriqueJC <pedrohenrique3317@outlook.com>
2023-03-19 11:51:37 -0300

    Códigos

---
+++ archives.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+#include <fstream> //Biblioteca para mexer com arquivos
+
+void main(){
+  int cont=0;
+  int c;
+  char conteudoTexto[255];
+  FILE *file;
+
+  file = fopen("Arquivo.txt","r"); //Abrindo o arquivo no modo leitura
+
+  if(file){ //Verificando se o arquivo foi aberto com sucesso
+    while((c = getc(file)) != EOF){ //Lendo o arquivo até o final(EOF)
+        printf("%c",c); //Imprime o caractere lido
+        conteudoTexto[cont] =c; //Armazena o caractere lido
+
+        cont++;
+
+      }
+
+  }
+
+  fclose(file); //Fecha o arquivo
+
+}
